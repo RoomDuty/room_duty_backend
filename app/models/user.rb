16425ab03add63efd2426b6b6c208bc6a3e77b2d
Rachel Lew <rlew07@gmail.com>
2020-09-24 14:39:15 -0500

    Add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates_presence_of :first_name, :last_name, :phone_number, :json_web_token, :role
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :password, require: true, :on => :create
+  validates_presence_of :password_confirmation, require: true, :on => :create
+
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,14 @@ require 'rails_helper'
 describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :phone_number}
+    it { should validate_presence_of :password }
+    it { should validate_presence_of :password_confirmation }
+    it { should validate_presence_of :json_web_token}
+    it { should validate_presence_of :role}
   end
 end
     
