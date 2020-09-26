0401bc47280367480c336b93c1022d65e8cbee13
Rachel Lew <rlew07@gmail.com>
2020-09-26 15:39:05 -0500

    Set up one to many relationship between floor and rooms

--- app/models/floor.rb
+++ app/models/floor.rb
@@ -1,3 +1,4 @@
 class Floor < ApplicationRecord
   belongs_to :user, optional: true
+  has_many :rooms
 end
--- spec/models/floor_spec.rb
+++ spec/models/floor_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Floor, type: :model do
   describe 'relationships' do
-    it { should belong_to(:user).optional}
+    it { should belong_to(:user).optional }
+    it { should have_many :rooms }
   end
 end
