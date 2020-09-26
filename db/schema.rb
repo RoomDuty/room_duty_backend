404a529d94f63385edaec8984fd638ae2c821c4b
Rachel Lew <rlew07@gmail.com>
2020-09-26 15:09:11 -0500

    Add user id as foreign key on floors table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_195705) do
+ActiveRecord::Schema.define(version: 2020_09_26_200438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_09_26_195705) do
   create_table "floors", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_floors_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema.define(version: 2020_09_26_195705) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "floors", "users"
 end
