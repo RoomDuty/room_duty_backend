3dfffe7899e3edfb0be3e9705eac76890688b50a
Rachel Lew <rlew07@gmail.com>
2020-09-26 15:38:48 -0500

    Create rooms table and add floor id as foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_200438) do
+ActiveRecord::Schema.define(version: 2020_09_26_203548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2020_09_26_200438) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_floors_on_user_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "floor_id"
+    t.index ["floor_id"], name: "index_rooms_on_floor_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +42,5 @@ ActiveRecord::Schema.define(version: 2020_09_26_200438) do
   end
 
   add_foreign_key "floors", "users"
+  add_foreign_key "rooms", "floors"
 end
